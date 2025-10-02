6f4c9695f5a29c814e0e038156689a7d2acde435
Sonya0904 <soph.zolotaryova01@gmail.com>
2025-10-02 18:50:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const scrollBtn = document.querySelector('.scroll-top');
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,           
+      behavior: 'smooth' 
+    });
+  });
+
+  
